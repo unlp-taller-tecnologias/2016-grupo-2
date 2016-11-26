b0e125c1755c487c6d0ea757ee72ca055f732e21
Alejo Marin Iparraguirre <alee_casta_95@hotmail.com>
2016-11-25 23:21:07 -0300

    correccion formato form reserva

--- SistemaHospital/src/AppBundle/Form/ReservaType.php
+++ SistemaHospital/src/AppBundle/Form/ReservaType.php
@@ -13,19 +13,55 @@ class ReservaType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('numeroReserva')
-        ->add('baja')
-        ->add('fecha_inicio')
-        ->add('fecha_fin')
-        ->add('paciente')
-        ->add('servicio')
-        ->add('estado')
-        ->add('quirofano', 'entity', array(
-                'multiple' => false,   // Multiple selection allowed
-                'expanded' => false,   // Render as checkboxes
+        $builder
+            ->add("fecha_inicio", "datetime",[
+                'label' => 'Fecha y Hora de Inicio',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add("fecha_fin", "datetime",[
+                'label' => 'Fecha y Hora de Finalización',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add("paciente", "choice",[
+                'label' => 'Paciente',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add("servicio", "choice",[
+                'label' => 'Servicio',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('paciente', 'entity', array(
+                'class' => 'AppBundle:Paciente',
+                'property'     => 'getNombreyApellido',
+                'label' => 'Paciente',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ))
+            ->add('servicio', 'entity', array(
+                'class' => 'AppBundle:Servicio',
+                'property'     => 'getTipo',
+                'label' => 'Servicio',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ))
+            ->add('quirofano', 'entity', array(
                 'class' => 'AppBundle:Quirofano',
-                'property'     => 'getNombre'
-            ))        ;
+                'property'     => 'getNombre',
+                'label' => 'Quirófano',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ));
     }
     
     /**
